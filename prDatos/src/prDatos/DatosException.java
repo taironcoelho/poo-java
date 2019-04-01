2cb811a1dcec6763088c04bc918200be8b755ef2
taironcoelho <taironcoelho@gmail.com>
2019-04-01 08:17:58 -0300

    Created project prDatos  references exercise 3.1

---
+++ prDatos/src/prDatos/DatosException.java
@@ -0,0 +1,13 @@
+package prDatos;
+
+public class DatosException extends RuntimeException{
+	
+	public DatosException() {
+		super();
+	}
+	
+	public DatosException(String msg) {
+		super(msg);
+	}
+
+}
